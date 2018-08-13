4cc37f4eb08bf0fede5dff2b763469d5e515fabf
ChrisDoernen <christian.doernen@icloud.com>
2018-08-13 19:20:58 +0200

    Fix ReSharper issues

--- LivestreamService/Server/Streaming/StreamingServerProcess.cs
+++ LivestreamService/Server/Streaming/StreamingServerProcess.cs
@@ -15,8 +15,8 @@ namespace Server.Streaming
         public StreamingServerProcess(string audioInput, int port)
         {
             _logger = LogManager.GetCurrentClassLogger();
-            this._audioInput = audioInput;
-            this._port = port;
+            _audioInput = audioInput;
+            _port = port;
             _command = $@"ffmpeg -y -f dshow -i audio=""{audioInput}"" -rtbufsize 64 -probesize 64 -acodec libmp3lame -ab 320k -ac 1 -reservoir 0 -f mp3 -hide_banner -fflags +nobuffer - | node NodeStreamingServer.js -port {port} -type mp3 -burstsize 0.1";
         }
         
