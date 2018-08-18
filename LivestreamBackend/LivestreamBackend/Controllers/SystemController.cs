66b2dc7cbbe6683708e25086ba660b271e60d509
ChrisDoernen <christian.doernen@icloud.com>
2018-08-18 19:14:31 +0200

    Add shutdown

---
+++ LivestreamBackend/LivestreamBackend/Controllers/SystemController.cs
@@ -0,0 +1,32 @@
+﻿using System.Management;
+using System.Web.Mvc;
+
+namespace LivestreamBackend.Controllers
+{
+    public class SystemController : Controller
+    {
+        // GET: System
+        public ActionResult Shutdown()
+        {
+            ManagementBaseObject mboShutdown = null;
+            ManagementClass mcWin32 = new ManagementClass("Win32_OperatingSystem");
+            mcWin32.Get();
+
+            // You can't shutdown without security privileges
+            mcWin32.Scope.Options.EnablePrivileges = true;
+            ManagementBaseObject mboShutdownParams =
+                mcWin32.GetMethodParameters("Win32Shutdown");
+
+            // Flag 1 means we want to shut down the system. Use "2" to reboot.
+            mboShutdownParams["Flags"] = "1";
+            mboShutdownParams["Reserved"] = "0";
+            foreach (ManagementObject manObj in mcWin32.GetInstances())
+            {
+                mboShutdown = manObj.InvokeMethod("Win32Shutdown",
+                    mboShutdownParams, null);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
