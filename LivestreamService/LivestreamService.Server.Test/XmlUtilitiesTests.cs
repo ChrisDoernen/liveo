a4224b8413f00b9b67ed85d4b68e2d2c06d231fc
ChrisDoernen <christian.doernen@icloud.com>
2018-09-01 12:32:44 +0200

    Add Server tests

---
+++ LivestreamService/LivestreamService.Server.Test/XmlUtilitiesTests.cs
@@ -0,0 +1,25 @@
+﻿using LivestreamService.Server.Entities;
+using LivestreamService.Server.Utilities;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LivestreamService.Server.Test
+{
+    [TestClass]
+    public class XmlUtilitiesTests
+    {
+        private readonly string ValidConfig = "LiveStreams.config";
+
+        [TestMethod]
+        public void ReadFromFile_ValidConfig()
+        {
+            // ARANGE
+
+            // ACT
+            var deserialized = XmlUtilities.ReadFromFile<LiveStreamsType>(ValidConfig);
+
+            // ASSERT
+
+        }
+    }
+}
