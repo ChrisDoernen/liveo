4ef3a8e9dcb7882ef4cc8c658e7aa3bdc8898ce0
ChrisDoernen <christian.doernen@icloud.com>
2018-09-04 21:35:19 +0200

    Remove api tests again

---
+++ Service/LivestreamApp.Api.Test/Tests/LivestreamModuleTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nancy;
+using Nancy.Testing;
+
+namespace LivestreamApp.Api.Test.Tests
+{
+    [TestClass]
+    public class LivestreamModuleTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Given
+            var bootstrapper = new DefaultNancyBootstrapper();
+            var browser = new Browser(bootstrapper);
+
+            // When
+            var result = browser.Get("/api/livestreams", with =>
+            {
+                with.HttpRequest();
+            });
+
+            // Then
+            Assert.Equals(HttpStatusCode.OK, result.StatusCode);
+
+        }
+    }
+}
