1e25a3ab00f27267121c134be93f2b6529cfc940
ChrisDoernen <christian.doernen@icloud.com>
2018-09-04 21:52:16 +0200

    Add dependencies

--- Service/LivestreamApp.Api.Test/Tests/LivestreamModuleTest.cs
+++ Service/LivestreamApp.Api.Test/Tests/LivestreamModuleTest.cs
@@ -3,7 +3,7 @@
 namespace LivestreamApp.Api.Test.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class LivestreamModuleTest
     {
         [TestMethod]
         public void TestMethod1()
