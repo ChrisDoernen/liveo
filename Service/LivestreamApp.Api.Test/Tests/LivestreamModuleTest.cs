514a10e51e0c5fdaf093494e10d5a4558f676b7b
ChrisDoernen <christian.doernen@icloud.com>
2018-09-04 23:22:55 +0200

    Green nancy test!

--- Service/LivestreamApp.Api.Test/Tests/LivestreamModuleTest.cs
+++ Service/LivestreamApp.Api.Test/Tests/LivestreamModuleTest.cs
@@ -1,4 +1,13 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FluentAssertions;
+using LivestreamApp.Api.Modules;
+using LivestreamApp.Server.Streaming;
+using LivestreamApp.Server.Streaming.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Nancy;
+using Nancy.Testing;
+using Ninject.Extensions.Logging;
+using System.Collections.Generic;
 
 namespace LivestreamApp.Api.Test.Tests
 {
@@ -6,10 +15,31 @@ namespace LivestreamApp.Api.Test.Tests
     public class LivestreamModuleTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GetLivestreams_ShouldGetStatusOKWithCorrectResponseJson()
         {
-            var i = true;
-            Assert.AreEqual(true, i);
+            // Given
+            var mockStreamingServer = new Mock<IStreamingServer>();
+            mockStreamingServer.Setup(mss => mss.GetStartedLiveStreams()).Returns(new List<Livestream>());
+            var mockLogger = new Mock<ILogger>();
+
+            var browser = new Browser(config =>
+                {
+                    config.Module<LivestreamsModule>();
+                    config.Dependency(mockStreamingServer.Object);
+                    config.Dependency(mockLogger.Object);
+                });
+
+            // When
+            var result = browser.Get("/api/livestreams", with =>
+            {
+                with.HttpRequest();
+                with.Header("Accept", "application/json");
+            });
+
+            // Then
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+            result.Body.ContentType.Should().Be("application/json; charset=utf-8");
+            result.Body.AsString().Should().Be("[]");
         }
     }
 }
