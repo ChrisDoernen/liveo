36b55deea1342cb19053a2484529d4531df70151
ChrisDoernen <christian.doernen@icloud.com>
2018-09-06 00:15:33 +0200

    Add backend module

--- Service/LivesteamApp.Service/AppConfiguration/NancyBootstrapper.cs
+++ Service/LivesteamApp.Service/AppConfiguration/NancyBootstrapper.cs
@@ -1,10 +1,18 @@
-﻿using Nancy.Bootstrappers.Ninject;
+﻿using Nancy.Bootstrapper;
+using Nancy.Bootstrappers.Ninject;
 using Ninject;
 
 namespace LivestreamApp.Service.AppConfiguration
 {
     public class NancyBootstrapper : NinjectNancyBootstrapper
     {
+        protected override void ApplicationStartup(IKernel container, IPipelines pipelines)
+        {
+            // No registrations should be performed in here, however you may
+            // resolve things that are needed during application startup.
+            this.Conventions.ViewLocationConventions.Add((viewName, model, context) => $"Views/{viewName}");
+        }
+
         protected override void ConfigureApplicationContainer(IKernel existingContainer)
         {
             // Perform registration that should have an application lifetime
---
+++ Service/LivestramApp.Backend/Modules/BackendModule.cs
@@ -0,0 +1,18 @@
+﻿using Nancy;
+using Ninject.Extensions.Logging;
+
+namespace LivestreamApp.Backend.Modules
+{
+    public class LivestreamsModule : NancyModule
+    {
+        public LivestreamsModule(ILogger logger)
+            : base("/backend")
+        {
+            Get["/"] = _ =>
+            {
+                logger.Info("/backend was invoked.");
+                return View["Index"];
+            };
+        }
+    }
+}
