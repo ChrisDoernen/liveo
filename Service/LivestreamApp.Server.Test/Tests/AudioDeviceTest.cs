7b28bd39b6083811e352511c8bc77bb7238c22f0
ChrisDoernen <christian.doernen@icloud.com>
2018-09-22 15:42:59 +0200

    Add Hardware and AudioDevice test

---
+++ Service/LivestreamApp.Server.Test/Tests/AudioDeviceTest.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using LivestreamApp.Server.Streaming.Environment;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LivestreamApp.Server.Test.Tests
+{
+    [TestClass]
+    public class AudioDeviceTest
+    {
+        private readonly AudioDevice _audioDevice = new AudioDevice("Mikro");
+
+        [TestMethod]
+        public void Equals_ShouldGetRightValueWhenNotTheSame()
+        {
+            // Given
+            var secondAudioDevice = new AudioDevice("Other mikro");
+
+            // When
+            var areSame = _audioDevice.Equals(secondAudioDevice);
+
+            // Then
+            areSame.Should().Be(false);
+        }
+
+        [TestMethod]
+        public void Equals_ShouldGetRightValueWhenTheSame()
+        {
+            // Given
+            var secondAudioDevice = _audioDevice;
+
+            // When
+            var areSame = _audioDevice.Equals(secondAudioDevice);
+
+            // Then
+            areSame.Should().Be(true);
+        }
+    }
+}
