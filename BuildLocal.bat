625d0f4c73d064125860ca7f0a7af742ea8b2d71
ChrisDoernen <christian.doernen@icloud.com>
2018-10-26 18:36:17 +0200

    Fix build file and add build local script

---
+++ BuildLocal.bat
@@ -0,0 +1,17 @@
+@echo off
+
+echo Restore nuget packages
+nuget Restore Service/Service.sln
+nuget Install NUnit.ConsoleRunner -Version 3.9.0 -OutputDirectory Service/TestRunner
+
+echo Build solution
+msbuild /p:Configuration=Release Service/Service.sln
+
+echo Run tests
+cd Service/TestRunner/NUnit.ConsoleRunner.3.9.0/tools
+nunit3-console.exe ../../../../Service/LivestreamApp.Api.Test/bin/Release/LivestreamApp.Api.Test.dll
+nunit3-console.exe ../../../../Service/LivestreamApp.Apps.Test/bin/Release/LivestreamApp.Apps.Test.dll
+nunit3-console.exe ../../../../Service/LivestreamApp.Server.Test/bin/Release/LivestreamApp.Server.Test.dll
+nunit3-console.exe ../../../../Service/LivestreamApp.Shared.Test/bin/Release/LivestreamApp.Shared.Test.dll
+
+pause
