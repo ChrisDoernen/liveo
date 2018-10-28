4156aac96021e11e8a573700a29a3ab1b5d71878
ChrisDoernen <christian.doernen@icloud.com>
2018-10-29 00:42:06 +0100

    Extend build local script and get unit test paths right in sonar config

--- BuildLocal.bat
+++ BuildLocal.bat
@@ -1,17 +1,31 @@
 @echo off
 
-echo Restore nuget packages
+echo Restore service nuget packages
 nuget Restore Service/Service.sln
 nuget Install NUnit.ConsoleRunner -Version 3.9.0 -OutputDirectory Service/TestRunner
 
-echo Build solution
+echo Build service solution
 msbuild /p:Configuration=Release Service/Service.sln
 
-echo Run tests
-cd Service/TestRunner/NUnit.ConsoleRunner.3.9.0/tools
-nunit3-console.exe ../../../../Service/LivestreamApp.Api.Test/bin/Release/LivestreamApp.Api.Test.dll^
- ../../../../Service/LivestreamApp.Apps.Test/bin/Release/LivestreamApp.Apps.Test.dll^
- ../../../../Service/LivestreamApp.Server.Test/bin/Release/LivestreamApp.Server.Test.dll^
- ../../../../Service/LivestreamApp.Shared.Test/bin/Release/LivestreamApp.Shared.Test.dll
+echo Run service tests
+"Service/TestRunner/NUnit.ConsoleRunner.3.9.0/tools/nunit3-console.exe"^
+ Service/LivestreamApp.Api.Test/bin/Release/LivestreamApp.Api.Test.dll^
+ Service/LivestreamApp.Apps.Test/bin/Release/LivestreamApp.Apps.Test.dll^
+ Service/LivestreamApp.Server.Test/bin/Release/LivestreamApp.Server.Test.dll^
+ Service/LivestreamApp.Shared.Test/bin/Release/LivestreamApp.Shared.Test.dll^
+ --result=Service/TestRunner/NUnitResults.xml
+
+echo Restore client npm packages
+cd Client
+call npm install
+
+echo Build client
+call ng build --prod --deploy-url /app/ --base-href /app/
+
+echo Run client tests
+call ng test --watch=false
+
+echo Copy client files
+Xcopy /E /I /Y dist "../Service/bin/Release/Client"
 
 pause
