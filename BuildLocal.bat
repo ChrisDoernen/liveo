50343bc3e1c5e40a96f8cfe072324e86349971e7
ChrisDoernen <christian.doernen@icloud.com>
2018-10-28 23:48:34 +0100

    Add test results to sonar cloud

--- BuildLocal.bat
+++ BuildLocal.bat
@@ -9,9 +9,9 @@ msbuild /p:Configuration=Release Service/Service.sln
 
 echo Run tests
 cd Service/TestRunner/NUnit.ConsoleRunner.3.9.0/tools
-nunit3-console.exe ../../../../Service/LivestreamApp.Api.Test/bin/Release/LivestreamApp.Api.Test.dll
-nunit3-console.exe ../../../../Service/LivestreamApp.Apps.Test/bin/Release/LivestreamApp.Apps.Test.dll
-nunit3-console.exe ../../../../Service/LivestreamApp.Server.Test/bin/Release/LivestreamApp.Server.Test.dll
-nunit3-console.exe ../../../../Service/LivestreamApp.Shared.Test/bin/Release/LivestreamApp.Shared.Test.dll
+nunit3-console.exe ../../../../Service/LivestreamApp.Api.Test/bin/Release/LivestreamApp.Api.Test.dll^
+ ../../../../Service/LivestreamApp.Apps.Test/bin/Release/LivestreamApp.Apps.Test.dll^
+ ../../../../Service/LivestreamApp.Server.Test/bin/Release/LivestreamApp.Server.Test.dll^
+ ../../../../Service/LivestreamApp.Shared.Test/bin/Release/LivestreamApp.Shared.Test.dll
 
 pause
--- Readme.md
+++ Readme.md
@@ -1,1 +1,2 @@
 [![Build Status](https://travis-ci.com/ChrisDoernen/Livestream-App.svg?branch=master)](https://travis-ci.com/ChrisDoernen/Livestream-App)
+[![Quality Status](https://sonarcloud.io/api/project_badges/measure?project=LivestreamApp&metric=alert_status)](https://sonarcloud.io/api/project_badges/measure?project=LivestreamApp&metric=alert_status)
