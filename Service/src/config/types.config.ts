58d1c385ee54bda242caeb05cfd9b98a66dfea5d
ChrisDoernen <christian.doernen@icloud.com>
2018-12-07 23:16:40 +0100

    Export types diffrently

--- Service/src/config/types.config.ts
+++ Service/src/config/types.config.ts
@@ -2,12 +2,10 @@ import { Logger } from "../core/util/logger";
 import { WindowsDeviceDetector } from "../core/system/devices/windows-device-detector";
 import { CommandExecutionService } from "../core/system/command-execution-service";
 
-const Types = {
+export const Types = {
     IShutdownService: Symbol.for("IShutdownService"),
     IDeviceDetector: Symbol.for("IDeviceDetector"),
     Logger: Logger,
     AudioInputDetector: WindowsDeviceDetector,
     CommandExecutionService: CommandExecutionService
 };
-
-export { Types };
