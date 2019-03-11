3daa40db10989502eeaa378bd6c939a41c6d1496
ChrisDoernen <christian.doernen@icloud.com>
2019-03-11 23:26:50 +0100

    Fix test

--- Client/src/app/components/home/home.component.spec.ts
+++ Client/src/app/components/home/home.component.spec.ts
@@ -7,20 +7,32 @@ import { InlineSVGService } from "ng-inline-svg/lib/inline-svg.service";
 import createMockInstance from "jest-create-mock-instance";
 import { async as _async } from "rxjs/scheduler/async";
 import { DataService } from "src/app/services/data/data.service";
+import { AudioPlayerComponent } from "../audio-player/audio-player.component";
+import { AboutComponent } from "../about/about.component";
+import { UserAgentService } from "src/app/services/user-agent/user-agent.service";
 
 describe("HomeComponent", () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
   let dataService: jest.Mocked<DataService>;
+  let userAgentService: jest.Mocked<UserAgentService>;
 
   beforeEach(() => {
     const inlineSVGService = jest.fn();
     dataService = createMockInstance(DataService);
+    userAgentService = createMockInstance(UserAgentService);
 
     TestBed.configureTestingModule({
-      declarations: [HomeComponent, HeaderComponent, InlineSVGDirective],
+      declarations: [
+        HomeComponent,
+        HeaderComponent,
+        InlineSVGDirective,
+        AudioPlayerComponent,
+        AboutComponent
+      ],
       providers: [
         { provide: DataService, useValue: dataService },
+        { provide: UserAgentService, useValue: userAgentService },
         { provide: InlineSVGService, useValue: inlineSVGService }
       ],
       imports: [RouterModule]
