3fa76ba5ae97b2071b9c5ead57247b093be04175
ChrisDoernen <christian.doernen@icloud.com>
2019-03-16 17:34:36 +0100

    Move data and log directory

--- Live/apps/service/src/app/config/service.config.ts
+++ Live/apps/service/src/app/config/service.config.ts
@@ -1,12 +1,18 @@
-import * as appRoot from "app-root-path";
+import * as appRoot from 'app-root-path';
 
 export const ServiceConfig = {
   os: process.platform,
   arch: process.arch,
   port: process.env.PORT ? +process.env.PORT : 3000,
-  development: process.env.DEVELOPMENT ? process.env.DEVELOPMENT.toLowerCase() == "true" : true,
-  simulate: process.env.SIMULATE ? process.env.SIMULATE.toLowerCase() == "true" : true,
-  sessions: `${appRoot}/dist/data/sessions.json`,
-  streams: `${appRoot}/dist/data/streams.json`,
-  autostart: `${appRoot}/dist/data/autostart.json`
+  development: process.env.DEVELOPMENT
+    ? process.env.DEVELOPMENT.toLowerCase() === 'true'
+    : true,
+  simulate: process.env.SIMULATE
+    ? process.env.SIMULATE.toLowerCase() === 'true'
+    : true,
+  sessions: `${appRoot}/dist/apps/service/assets/data/sessions.json`,
+  streams: `${appRoot}/dist/apps/service/assets/data/streams.json`,
+  autostart: `${appRoot}/dist/apps/service/assets/data/autostart.json`,
+  logfilename: `${appRoot}/dist/apps/service/logs/live-service.log`,
+  ffmpeglogfilename: `${appRoot}/dist/apps/service/logs/live-ffmpeg.log`
 };
