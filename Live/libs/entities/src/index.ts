dcd6aff829949b07855629d2263f06d61b460302
ChrisDoernen <christian.doernen@icloud.com>
2019-03-24 11:57:56 +0100

    Add index file per directory

--- Live/libs/entities/src/index.ts
+++ Live/libs/entities/src/index.ts
@@ -1,5 +1,1 @@
-export { ActivationEntity } from './lib/activation.entity';
-export { ActivationState } from './lib/activation-state';
-export { SessionEntity } from './lib/session.entity';
-export { StreamType } from './lib/stream-type';
-export { StreamEntity } from './lib/stream.entity';
+export * from './lib';
