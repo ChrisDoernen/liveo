54bb9dc219fb6b8c36adbb6d1800918915eb924c
ChrisDoernen <christian.doernen@icloud.com>
2019-04-26 21:23:00 +0200

    Add rimraf

--- Live/apps/service/src/main.ts
+++ Live/apps/service/src/main.ts
@@ -1,6 +1,6 @@
-import 'reflect-metadata'; // This has to be imported first
-import { container } from './app/config/container';
-import { Bootstrapper } from './app/lib/core/bootstrapper';
+import "reflect-metadata"; // This has to be imported first
+import { container } from "./app/config/container";
+import { Bootstrapper } from "./app/lib/core/bootstrapper";
 
-const bootstrapper = container.get<Bootstrapper>('Bootstrapper');
+const bootstrapper = container.get<Bootstrapper>("Bootstrapper");
 bootstrapper.startServer(container);
