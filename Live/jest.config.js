897b379fc4964ee32635ec1c487707383ce3ce09
Christian Dörnen <christian.doernen@icloud.com>
2019-06-21 16:48:07 +0200

    Fix tests

--- Live/jest.config.js
+++ Live/jest.config.js
@@ -1,11 +1,10 @@
 const esModules = ['ngx-socket-io'].join('|');
 
 module.exports = {
-  preset: "jest-preset-angular",
   testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
   transform: {
     [`(${esModules}).+\\.js$`]: 'babel-jest',
-    '^.+\\.(ts|html)$': 'ts-jest'
+    '^.+\\.(ts|js|html)$': 'ts-jest'
   },
   resolver: '@nrwl/builders/plugins/jest/resolver',
   moduleFileExtensions: ['ts', 'js', 'html'],
