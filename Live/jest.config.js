5b53cd93698011260ba8af5a168d9750e9113b5b
Christian Dörnen <christian.doernen@icloud.com>
2019-06-21 16:32:38 +0200

    Workaround for jest with ngx-socket-io

--- Live/jest.config.js
+++ Live/jest.config.js
@@ -1,10 +1,15 @@
+const esModules = ['ngx-socket-io'].join('|');
+
 module.exports = {
+  preset: "jest-preset-angular",
   testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
   transform: {
-    '^.+\\.(ts|js|html)$': 'ts-jest'
+    [`(${esModules}).+\\.js$`]: 'babel-jest',
+    '^.+\\.(ts|html)$': 'ts-jest'
   },
   resolver: '@nrwl/builders/plugins/jest/resolver',
   moduleFileExtensions: ['ts', 'js', 'html'],
   collectCoverage: true,
-  coverageReporters: ['html']
+  coverageReporters: ['html'],
+  transformIgnorePatterns: [`/node_modules/(?!${esModules})`]
 };
