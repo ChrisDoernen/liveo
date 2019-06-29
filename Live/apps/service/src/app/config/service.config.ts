3497a98b91a351633468b7ad07ed63a63bc48464
Christian Dörnen <christian.doernen@icloud.com>
2019-06-29 13:04:47 +0200

    Disable dummy navigation

--- Live/apps/service/src/app/config/service.config.ts
+++ Live/apps/service/src/app/config/service.config.ts
@@ -9,7 +9,7 @@ export const config = {
   simulate: process.env.SIMULATE ? process.env.SIMULATE === "true" : environment.simulate,
   filesource: process.env.FILESOURCE ? process.env.FILESOURCE === "true" : environment.filesource,
   standalone: process.env.STANDALONE ? process.env.STANDALONE === "true" : environment.standalone,
-  database: `${appRoot}/dist/apps/service/assets/data/db.json`,
-  logfilename: `${appRoot}/dist/apps/service/logs/live-service.log`,
-  ffmpeglogfilename: `${appRoot}/dist/apps/service/logs/live-ffmpeg.log`
+  database: process.env.DBFILE ? process.env.DBFILE : `${appRoot}/dist/apps/service/assets/data/db.json`,
+  logfilename: process.env.LOGFILE ? process.env.LOGFILE : `${appRoot}/dist/apps/service/logs/live-service.log`,
+  ffmpeglogfilename: process.env.FFMPEGLOGFILE ? process.env.FFMPEGLOGFILE : `${appRoot}/dist/apps/service/logs/live-ffmpeg.log`
 };
