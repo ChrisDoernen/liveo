b3f0831634e3ca2d201d583ae1cd921b5d9b343a
ChrisDoernen <christian.doernen@icloud.com>
2019-07-20 22:24:13 +0200

    Get command working

--- Live/apps/service/src/app/lib/streaming-sources/streaming-source.ts
+++ Live/apps/service/src/app/lib/streaming-sources/streaming-source.ts
@@ -4,14 +4,14 @@ import { Device } from "../devices/device";
 import { DeviceState } from "../devices/device-state";
 import { WebsocketServer } from "../core/websocket-server";
 import { EVENTS } from "@live/constants";
-import * as Ffmpeg from "fluent-ffmpeg";
+const FfmpegCommand = require("fluent-ffmpeg");
 
 /**
  * Class responsible for opening a child process and passing the data to the websocket server
  */
 @injectable()
 export class StreamingSource {
-  private _command: Ffmpeg.FfmpegCommand;
+  private _command: any;
   public isStreaming: boolean;
 
   constructor(
@@ -20,48 +20,57 @@ export class StreamingSource {
     @inject("WebsocketService") private _websocketServer: WebsocketServer,
     private _device: Device,
     private _streamId: string) {
-    this._command = new Ffmpeg.FfmpegCommand()
-      .input(this._device.id)
-      .inputOptions("-y")
-      .inputOptions("-f")
-      .audioChannels(2)
-      .audioBitrate("196k")
-      .audioCodec("lib3lame")
-      .format("mp3")
-      .outputOptions(["-probesize 64"])
-      .outputOptions(["-rtbufsize 64"])
-      .outputOptions(["-resorvoir 0"])
-      .outputOptions(["-fflags"])
-      .outputOptions(["+nobuffer"])
-      .outputOptions(["-hide_banner"])
-      .on("start", (command) => {
-        this._websocketServer.addStream(this._streamId);
-        this._logger.debug(`Started streaming for device ${this._device.id} with command ${command}.`);
-        this.isStreaming = true;
-      })
-      .on("error", (error) => this._logger.error(`Error ffmpeg command for device ${this._device.id}: ${error}.`))
-      .on("stderr", (data) => this._ffmpegLogger.info(`${data}`))
-      .on("end", () => {
-        this._websocketServer.removeStream(this._streamId);
-        this._websocketServer.emitEventMessage(this._streamId, EVENTS.streamEnded, "The stream ended.");
-        this._logger.info(`Child process for device ${this._device.id}.`);
-        this.isStreaming = false;
-      })
+    this.initializeFfmpegCommand();
   }
 
   public get hasValidDevice(): boolean {
     return this._device.state !== DeviceState.UnknownDevice;
   }
 
+  private initializeFfmpegCommand(): void {
+    this._command = new FfmpegCommand()
+      .input(this._device.id)
+      .inputOptions("-y")
+      .inputOptions("-f alsa")
+      .audioChannels(2)
+      .audioBitrate("196k")
+      .audioCodec("libmp3lame")
+      .format("mp3")
+      .outputOptions(["-probesize 64"])
+      .outputOptions(["-rtbufsize 64"])
+      .outputOptions(["-reservoir 0"])
+      .outputOptions(["-fflags"])
+      .outputOptions(["+nobuffer"])
+      .outputOptions(["-hide_banner"])
+      .on("start", (command: string) => {
+        this._logger.debug(`Started streaming for device ${this._device.id} with command ${command}.`);
+      })
+      .on("error", (error: Error) => {
+        // We killed the stream manually to stop streaming, no real error
+        if (!error.message.includes("SIGKILL")) {
+          this._logger.error(`Error ffmpeg command for device ${this._device.id}: ${error}.`);
+        }
+      })
+      .on("stderr", (data: string) => {
+        this._ffmpegLogger.info(`${data}`);
+      })
+      .on("end", () => { });
+  }
+
   public startStreaming(): void {
     this._logger.debug(`Start streaming for device ${this._device.id}.`);
+    this._websocketServer.addStream(this._streamId);
     this._command
       .pipe()
       .on("data", (data: Buffer) => this._websocketServer.emitStreamData(this._streamId, data));
+    this.isStreaming = true;
   }
 
   public stopStreaming(): void {
     this._logger.debug(`Killing child process for device ${this._device.id}.`);
     this._command.kill("SIGKILL");
+    this._websocketServer.removeStream(this._streamId);
+    this._websocketServer.emitEventMessage(this._streamId, EVENTS.streamEnded, "The stream ended.");
+    this.isStreaming = false;
   }
 }
