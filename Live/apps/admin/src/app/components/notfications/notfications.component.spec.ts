3dd6ccdfa533cbc079889fbb2812765e0eda458a
ChrisDoernen <christian.doernen@icloud.com>
2019-08-16 15:41:25 +0200

    Adapt test

--- Live/apps/admin/src/app/components/notfications/notfications.component.spec.ts
+++ Live/apps/admin/src/app/components/notfications/notfications.component.spec.ts
@@ -1,18 +1,21 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { NotficationsComponent } from "./notfications.component";
+import { AngularMaterialModule } from "../../angular-material.module";
 
 describe("NotficationsComponent", () => {
   let component: NotficationsComponent;
   let fixture: ComponentFixture<NotficationsComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [NotficationsComponent]
-    })
-      .compileComponents();
-  }));
-
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        AngularMaterialModule
+      ],
+      declarations: [
+        NotficationsComponent
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(NotficationsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
