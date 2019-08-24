368680ac7f4be22f8c5d2be0c750fb125dcf5446
ChrisDoernen <christian.doernen@icloud.com>
2019-08-24 20:24:49 +0200

    Fix test

--- Live/apps/client/src/app/services/activity/application-state.service.spec.ts
+++ Live/apps/client/src/app/services/activity/application-state.service.spec.ts
@@ -1,14 +1,22 @@
 import { TestBed } from "@angular/core/testing";
 import { ApplicationStateService } from "./application-state.service";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { EndpointService } from "@live/services";
+import createMockInstance from "jest-create-mock-instance";
 
 describe("ApplicationStateService", () => {
   let applicationStateService: ApplicationStateService;
+  let endpointService: jest.Mocked<EndpointService>;
 
   beforeEach(() => {
+    endpointService = createMockInstance(EndpointService);
+
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [ApplicationStateService]
+      providers: [
+        ApplicationStateService,
+        { provide: EndpointService, useValue: endpointService }
+      ]
     });
 
     applicationStateService = TestBed.get(ApplicationStateService);
