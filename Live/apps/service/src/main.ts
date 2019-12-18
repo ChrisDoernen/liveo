6d7f4e288a423bfbea7dc5f187e42a1ec256f6aa
ChrisDoernen <christian.doernen@icloud.com>
2019-12-18 17:43:16 +0100

    Add dotenv

--- Live/apps/service/src/main.ts
+++ Live/apps/service/src/main.ts
@@ -1,6 +1,14 @@
 import "reflect-metadata"; // This has to be imported first
 import { container } from "./app/config/container";
 import { Bootstrapper } from "./app/core/bootstrapper";
+import { join } from "path";
+import { config } from "dotenv";
+
+const envPath = join(process.cwd(), "live.config");
+const dotenv = config({ path: envPath, debug: true });
+if (dotenv.error) {
+  throw dotenv.error;
+}
 
 const bootstrapper = container.get<Bootstrapper>("Bootstrapper");
 bootstrapper.startServer(container);
