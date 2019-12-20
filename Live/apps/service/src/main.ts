7520a17902883394c611891f11b5332c394551f9
ChrisDoernen <christian.doernen@icloud.com>
2019-12-20 14:23:32 +0100

    Parese env file at the very fist

--- Live/apps/service/src/main.ts
+++ Live/apps/service/src/main.ts
@@ -1,14 +1,7 @@
 import "reflect-metadata"; // This has to be imported first
+import "./dotenv";
 import { container } from "./app/config/container";
 import { Bootstrapper } from "./app/core/bootstrapper";
-import { join } from "path";
-import { config } from "dotenv";
-
-const envPath = join(process.cwd(), "live.env");
-const dotenv = config({ path: envPath, debug: true });
-if (dotenv.error) {
-  throw dotenv.error;
-}
 
 const bootstrapper = container.get<Bootstrapper>("Bootstrapper");
 bootstrapper.startServer(container);
