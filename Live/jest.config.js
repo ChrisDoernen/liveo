b5f670fbb8e10c51af15380cece20d2fcc47a66d
ChrisDoernen <christian.doernen@icloud.com>
2019-12-22 14:27:32 +0100

    Make tests green

--- Live/jest.config.js
+++ Live/jest.config.js
@@ -1,4 +1,4 @@
-const esModules = ["ngx-socket-io", "ng-inline-svg"].join("|");
+const esModules = ["ng-inline-svg"].join("|");
 
 module.exports = {
   testMatch: ["**/+(*.)+(spec|test).+(ts|js)?(x)"],
