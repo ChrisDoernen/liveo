e4866cbab769e714f3acda95d6edee9ba7180a0c
ChrisDoernen <christian.doernen@icloud.com>
2020-01-04 15:58:55 +0100

    Fix test

--- Live/apps/admin/src/app/modules/streams/components/stream-creation/stream-creation.component.spec.ts
+++ Live/apps/admin/src/app/modules/streams/components/stream-creation/stream-creation.component.spec.ts
@@ -3,13 +3,16 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MatDialog } from "@angular/material";
 import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { ActivatedRoute } from "@angular/router";
+import { RouterTestingModule } from "@angular/router/testing";
 import { EndpointService, Logger } from "@live/services";
 import createMockInstance from "jest-create-mock-instance";
 import { SessionService } from "../../../../services/session/session.service";
 import { AngularMaterialModule } from "../../../angular-material/angular-material.module";
+import { TitleBarComponent } from "../../../shared/components/title-bar/title-bar.component";
 import { StreamCreationComponent } from "./stream-creation.component";
 
-describe("StreamCreationDialogComponent", () => {
+describe("StreamCreationComponent", () => {
   let component: StreamCreationComponent;
   let fixture: ComponentFixture<StreamCreationComponent>;
   let endpointService: jest.Mocked<EndpointService>;
@@ -23,17 +26,22 @@ describe("StreamCreationDialogComponent", () => {
       imports: [
         HttpClientModule,
         AngularMaterialModule,
+        RouterTestingModule,
         FormsModule,
         ReactiveFormsModule,
         NoopAnimationsModule,
         AngularMaterialModule
       ],
-      declarations: [StreamCreationComponent],
+      declarations: [
+        StreamCreationComponent,
+        TitleBarComponent
+      ],
       providers: [
         { provide: MatDialog, useValue: jest.fn() },
         { provide: SessionService, useValue: sessionService },
         { provide: EndpointService, useValue: endpointService },
-        { provide: Logger, useValue: jest.fn() }
+        { provide: Logger, useValue: jest.fn() },
+        { provide: ActivatedRoute, useValue: createMockInstance(ActivatedRoute) }
       ]
     }).compileComponents();
 
--- Live/apps/admin/src/app/modules/streams/components/stream-creation/stream-creation.component.ts
+++ Live/apps/admin/src/app/modules/streams/components/stream-creation/stream-creation.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { ActivatedRoute, Router } from "@angular/router";
 import { DeviceEntity, StreamEntity, StreamType } from "@live/entities";
-import { StreamService } from "apps/admin/src/app/services/stream/stream.service";
 import { DevicesService } from "../../../../services/devices/devices.service";
+import { StreamService } from "../../../../services/stream/stream.service";
 
 @Component({
   selector: "stream-creation",
