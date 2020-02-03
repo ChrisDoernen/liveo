29619238e30ff6b42af6c14281a3aaf5597e3b2d
ChrisDoernen <christian.doernen@icloud.com>
2020-02-03 12:43:37 +0100

    Settings data builder

--- Live/apps/service/src/app/services/data/data-service.spec.ts
+++ Live/apps/service/src/app/services/data/data-service.spec.ts
@@ -1,15 +1,18 @@
-import "reflect-metadata";
-import { DataService } from "./data-service";
-import { Logger } from "../logging/logger";
 import createMockInstance from "jest-create-mock-instance";
+import "reflect-metadata";
+import { IdGenerator } from "../id-generation/id-generator";
+import { Logger } from "../logging/logger";
+import { DataService } from "./data-service";
 
 describe("DataService", () => {
   let dataService: DataService;
   let logger: jest.Mocked<Logger>;
+  let idGenerator: jest.Mocked<IdGenerator>;
 
   beforeEach(() => {
     logger = createMockInstance(Logger);
-    dataService = new DataService(logger);
+    idGenerator = createMockInstance(IdGenerator);
+    dataService = new DataService(logger, idGenerator);
   });
 
   it("should construct", () => {
--- Live/libs/test-utilities/src/lib/index.ts
+++ Live/libs/test-utilities/src/lib/index.ts
@@ -1,6 +1,7 @@
 export { LoggerMock } from "./mocks/logger-mock";
 export { ActivationEntityBuilder } from "./test-data-builder/activation-entity-builder";
 export { SessionEntityBuilder } from "./test-data-builder/session-entity-builder";
+export { SettingsEntityBuilder } from "./test-data-builder/settings-entity-builder";
 export { StreamEntityBuilder } from "./test-data-builder/stream-entity-builder";
 export { UserEntityBuilder } from "./test-data-builder/user-entity-builder";
 
