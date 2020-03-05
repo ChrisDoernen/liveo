fe3269d26b1fa642b6266d0a4b4912a99cc5365b
ChrisDoernen <christian.doernen@icloud.com>
2020-03-05 19:57:52 +0100

    Work on nightly build deployment

---
+++ Live/scripts/collect-artifacts.ts
@@ -0,0 +1,61 @@
+// tslint:disable: no-shadowed-variable
+import { existsSync, mkdirSync, writeFile, writeFileSync } from "fs";
+import * as JSZip from "jszip";
+import { join } from "path";
+import { version } from "../version";
+
+interface ArtifactInfo {
+  date: string;
+  branch: string;
+  revision: string;
+  version: string;
+}
+
+const createDirectoryIfNotExisting = (directory: string): void => {
+  if (!existsSync(directory)) {
+    mkdirSync(directory);
+  }
+}
+
+const checkArguments = (): string => {
+  const artifactDirectory = process.argv[2];
+
+  if (!artifactDirectory) {
+    console.error("Please specify a directory");
+    process.exit(1);
+  }
+
+  return artifactDirectory;
+}
+
+
+const zipWinX64Build = (directory: string) => {
+  const zip = new JSZip();
+  zip.folder("bin/win-x64").generateAsync({ type: "uint8array" }).then((content) => {
+    const fileName = join(directory, "win-x64.zip");
+    writeFile(fileName, content, () => {
+      console.log(`Wrote file ${fileName}`);
+    });
+  });
+}
+
+const getArtifactInfoJson = (): ArtifactInfo => {
+  return {
+    date: new Date().toISOString(),
+    branch: version.branch,
+    revision: version.revision,
+    version: version.version
+  };
+}
+
+const writeArtifactInfo = (directory: string, artifactInfo: ArtifactInfo) => {
+  const fileName = join(directory, "artifactInfo.json");
+  writeFileSync(fileName, JSON.stringify(artifactInfo, undefined, 2));
+  console.log(`Wrote build info to ${fileName} with content ${JSON.stringify(artifactInfo)}`);
+}
+
+const artifactDirectory = checkArguments();
+createDirectoryIfNotExisting(artifactDirectory);
+const artifactInfo = getArtifactInfoJson();
+writeArtifactInfo(artifactDirectory, artifactInfo);
+zipWinX64Build(artifactDirectory);
